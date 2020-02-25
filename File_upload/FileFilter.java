bd3f051ee98b4acbe0f73fa5ba35e78e1948c5cc
LeeJuWan <49786050+LeeJuWan@users.noreply.github.com>
2020-02-26 03:57:33 +0900

    2020.02.26 error repair & mime type update

--- File_upload/FileFilter.java
+++ File_upload/FileFilter.java
@@ -1,10 +1,6 @@
 package your package;
 
-import org.apache.tika.Tika;
-// 위의 라이브러리는 maven repo https://mvnrepository.com/artifact/org.apache/tika/0.2 에서 다운받으세요.
-
 import java.io.File;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
@@ -15,47 +11,46 @@ import java.net.URLDecoder;
 
 public class FileFilter {
 
-    public static boolean fileFilter(File fileData){ // 파라미터를 통해 파일 데이터를 입력 시, 참 / 거짓으로 악성유무 판단
-    
+    public static boolean fileFilter(File fileData){
         final int fileAllowSize= 10240000; //10MB이하
-        boolean result= false; //default 셋팅
+        boolean result= true; //default 셋팅
 
-        Tika tika = new Tika(); // MIME 타입 확인을 위해 tika 생성
-
-        if(fileData != null && "".equals(fileData.getName())){ // null 체크 및 공백 체크
+        if(!"".equals(fileData.getName())){
             String file= "";
             try{
-                file= URLDecoder.decode(fileData.getName(),"euc-kr"); // 1단계 인코딩 우회 방지
+                file= URLDecoder.decode(fileData.getName(),"euc-kr"); // 1단계 인코딩 우회 검증
 
-                if(fileData.length()>fileAllowSize || fileData.canExecute()) // 2단계 파일의 사이즈 & 파일 실행여부 확인
-                    result = true; // 위험한 형식의 파일 발견
-                else{
-                    if(file.toLowerCase().endsWith(".jpg") || file.toLowerCase().endsWith(".png") ||
-                    file.toLowerCase().endsWith(".jpeg"))
-                    { // 3단계 문자열의 마지막 확장자 확인
-                        // test.jpg.asp의 '.asp'를 막기위해 파일 전체이름 리딩
-                        // 리딩된 파일 이름을 통해 끝부분의 확장자 .jpg 또는 .png가 맞다면 2단계 검증 완료
+                if(fileData.length()>fileAllowSize) { // 2단계 파일의 사이즈
+                    result = true;
+                }else{
+                    if(fileData.canExecute()){ // 3단계 파일 실행여부 검증
+                        result = true;
+                    }else{
 
-                        String mimeType = tika.detect(fileData); // 4단계 MIME type 확인
-                        if(mimeType.startsWith("image")){
-                            // test.txt->test.jpg로 변환 가능
-                            // 변환된 test.jpg 파일의 mime type은 -> text/plane
-                            result = false; // 파일 확장자 안전함
-                        }
-                        else
+                        String [] array=file.split("\\."); // ex) test.jsp%00.jpg 검출목적
+                        if(array[1].toLowerCase().endsWith("jpg") || file.toLowerCase().endsWith("png") ||
+                                file.toLowerCase().endsWith("jpeg")) // 4단계 문자열의 마지막 확장자 검증
+                        {
+                            // 5단계 mime type 검증, mime type 변환 진행
+                            MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+                            String extenstion = MimeTypeMap.getFileExtensionFromUrl(fileData.getName());
+                            String mime_Type = mimeTypeMap.getMimeTypeFromExtension(extenstion);
+
+                            if(mime_Type.toLowerCase().startsWith("image")){
+                                result = false; // 안전한 파일
+                            } else // mime 타입 없을 시 null 반환
+                                result = true; // 위험한 형식의 파일 발견
+
+                        }else
                             result = true; // 위험한 형식의 파일 발견
-                    }else
-                        result = true; // 위험한 형식의 파일 발견
-
+                    }
                 }
             }catch (UnsupportedEncodingException e) {
-                System.err.println("FileFilter error");
-            } catch (IOException e) {
-                System.err.println("FileFilter IOExcepiont error");
+                System.err.println("FileFilter UnsupportedEncodingException error");
             }
         }
         else {
-           result =true; //파일 null & 공백 파일
+            result =true; //공백 파일
         }
         return  result;
     }
